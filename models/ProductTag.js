0778ae6038d9a9bac9d134c2b926e8578f14dd09
Tracy Pham <phamtracy95@gmail.com>
2022-11-22 21:30:16 -0800

    Add Product Tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,41 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: dataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_id: {
+      type: dataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+        unique: false,
+      },
+    },
+
+    tag_id: {
+      type: dataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
